9a243f558e4628a464a9d97055c0c59be3de1f4f
Chris McComb <ccmcc2012@gmail.com>
2021-11-11 12:37:08 -0500

    For some reason 'predict' is failing locally, but it might work in CI.

--- src/regression.rs
+++ src/regression.rs
@@ -1,9 +1,10 @@
-use linfa::dataset::{AsTargets, DatasetBase};
-use linfa::prelude::SingleTargetRegression;
+use eval_metrics::regression::{mae, mse, rmse, rsq};
+use linfa::dataset::{AsTargets, DatasetBase, Records};
+use linfa::prelude::*;
 use linfa::traits::{Fit, Predict};
 use linfa_elasticnet::ElasticNet;
 use linfa_linear::{FittedLinearRegression, Float, LinearRegression};
-use ndarray::{Array1, Array2, ArrayBase, Axis, Data, DataMut, Ix1, Ix2};
+use ndarray::{Array1, Array2, ArrayBase, Axis, Data, DataMut, Dim, Ix1, Ix2, OwnedRepr};
 
 pub struct Settings {
     cv: u64,
@@ -24,13 +25,19 @@ impl<F: Float> Regressor for ElasticNet<F> {}
 /// let data = linfa_datasets::diabetes();
 /// automl::regression::compare_models(&data, automl::regression::Settings::default());
 /// ```
-pub fn compare_models<F: Float, D: Data<Elem = F>, T: AsTargets<Elem = F>>(
+pub fn compare_models<F: Float, D: Data<Elem = F>, T: AsTargets<Elem = F>, R: Records>(
     dataset: &DatasetBase<ArrayBase<D, Ix2>, T>,
     settings: Settings,
-) -> Vec<Box<dyn Regressor>> {
-    let mut results: Vec<Box<dyn Regressor>> = Vec::new();
-    results.push(Box::new(LinearRegression::default().fit(dataset).unwrap()));
-    results.push(Box::new(ElasticNet::ridge().fit(dataset).unwrap()));
-    results.push(Box::new(ElasticNet::lasso().fit(dataset).unwrap()));
-    results
+) -> Vec<Box<dyn Predict<R, T>>> {
+    let mut models: Vec<Box<dyn Predict<R, T>>> = Vec::new();
+    models.push(Box::new(LinearRegression::default().fit(dataset).unwrap()));
+    models.push(Box::new(ElasticNet::ridge().fit(dataset).unwrap()));
+    models.push(Box::new(ElasticNet::lasso().fit(dataset).unwrap()));
+
+    // let r2 = Vec::new();
+    // for model in models {
+    // model.
+    // }
+
+    models
 }
